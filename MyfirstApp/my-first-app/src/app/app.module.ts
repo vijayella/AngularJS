90783b16aebac1e1ea17690bcae2ce11d410ee44
Vijay <yella.vijaykumar@gmail.com>
2017-08-29 17:50:08 +0530

    Added ToDo App code

--- MyfirstApp/my-first-app/src/app/app.module.ts
+++ MyfirstApp/my-first-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
