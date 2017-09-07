b84bc9d85cf2700753ce26214e0cd22468cfa938
Vijay <yella.vijaykumar@gmail.com>
2017-09-07 15:46:08 +0530

    modified document

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent
+    AppComponent
   ],
+  exports:[AppComponent],
   imports: [
     BrowserModule
   ],
@@ -18,3 +15,4 @@ import { NavbarComponent } from './navbar/navbar.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
